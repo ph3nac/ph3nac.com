dd892cd3c7ba7a84cf249a595d59877edc18652a
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-12-24 07:39:05 +0900

    update shell

--- newpost.sh
+++ newpost.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 echo \
-" \
----\n \
-title: ''\n \
-metaTitle: ''\n \
-metaDesc: ''\n \
-socialImage: images/\n \
-date: '$(date +%Y-%m-%d)'\n \
-tags:\n \
-  - \n \
---- \
-" \
+"\
+---\n\
+title: ''\n\
+metaTitle: ''\n\
+metaDesc: ''\n\
+socialImage: images/\n\
+date: '$(date +%Y-%m-%d)'\n\
+tags:\n\
+  - \n\
+---\
+\n
+main
+"\
 > posts/$(date +%Y%m%d%H%M%S).md
 
